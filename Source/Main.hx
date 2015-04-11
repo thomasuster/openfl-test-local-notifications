07f3c432801f99211236405b746ef04aa0ef8d6e
Thomas Uster <thomasuster@gmail.com>
2015-04-11 08:15:03 -0700

    Added notification. Verified on android.

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,6 +1,9 @@
 package;
 
-
+import com.thomasuster.IOSLocalNotifications;
+import com.thomasuster.LocalNotifications;
+import com.thomasuster.AndroidLocalNotifications;
+import com.thomasuster.Notification;
 import openfl.display.Bitmap;
 import openfl.display.BitmapData;
 import openfl.display.Sprite;
@@ -19,6 +22,23 @@ class Main extends Sprite {
 		
 		bitmap.x = (stage.stageWidth - bitmap.width) / 2;
 		bitmap.y = (stage.stageHeight - bitmap.height) / 2;
+
+		var localNotofications:LocalNotifications;
+
+        #if android
+        localNotofications = new AndroidLocalNotifications();
+        #elseif ios
+        localNotofications = new IOSLocalNotifications();
+        #else
+        return;
+        #end
+
+        var notification:Notification = new Notification();
+        notification.id = 0;
+        notification.title = 'Eureka!';
+        notification.textContent = 'Hello Hugh';
+        notification.milliseconds = 0;
+        localNotofications.schedule(notification);
 		
 	}
 	
